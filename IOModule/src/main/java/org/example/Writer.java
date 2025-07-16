578701a2ef96cdbfea5ffeba5af3c06ff3b0be93
Rin Zya <Arina.Zyazina@yandex.ru>
2025-07-16 11:02:21 +0300

    add logs

--- IOModule/src/main/java/org/example/Writer.java
+++ IOModule/src/main/java/org/example/Writer.java
@@ -1,33 +1,56 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class Writer {
+    private static final Logger logger = LogManager.getLogger(Writer.class);
     public static void PrintToConsole(Sudoku sudoku) {
         System.out.println("Решение заданного судоку:");
         String strSudoku = sudoku.toString();
         System.out.println(strSudoku);
+        logger.info("Судоку напечатано на экран");
     }
 
-    public static void PrintToTextFile(Sudoku sudoku, String fileName, String message) {
+    public static void PrintToTextFile(Sudoku sudoku) {
+        String fileName = InputNotExistFileName();
+
         try {
             FileWriter writer = new FileWriter(fileName);
             writer.write(sudoku.toString());
             writer.close();
-            System.out.println(message);
+
+            String mes = "Запись в файл " + fileName + " завершена";
+            logger.info(mes);
+            System.out.println(mes);
         } catch (IOException e) {
-            System.out.println("Ошибка записи в файл: " + e.getMessage());
+            String mes = "Ошибка записи в файл: " + e.getMessage();
+            logger.error(mes);
+            System.out.println(mes);
         }
+    }
+    private static String InputNotExistFileName() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите имя нового текстового файла для записи данных");
+
+        String curFileName = null;
+        boolean isExist = true;
+
+        while (isExist) {
+            curFileName = sc.nextLine();
+            File curFile = new File(curFileName);
+            isExist = curFile.exists();
+            if (isExist) {
+                logger.info("Попытка ввода имени существующего файла для записи, запрошен повтор ввода");
+                System.out.println("Файл с таким именем уже существует. Повторите ввод...");
+            }
+        }
+        logger.info("Успешный ввод имени файла");
+        return curFileName;
     }
 
-    public static void PrintToTextFile(Sudoku sudoku, String fileName) {
-        try {
-            FileWriter writer = new FileWriter(fileName);
-            writer.write(sudoku.toString());
-            writer.close();
-            System.out.println("Запись судоку в текстовый файл завершена.");
-        } catch (IOException e) {
-            System.out.println("Ошибка записи в файл: " + e.getMessage());
-        }
-    }
 }
--- SudokuModule/src/main/java/org/example/Sudoku.java
+++ SudokuModule/src/main/java/org/example/Sudoku.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.InputMismatchException;
 
 public class Sudoku {
     static public final int MAX_LENGTH = 9;
     static public final int BOX_LENGTH = 3;
 
+    private static final Logger logger = LogManager.getLogger(Sudoku.class);
     private final int[][] board;
 
     public Sudoku(char[][] inputArr) {
@@ -18,11 +22,14 @@ public class Sudoku {
                     board[i][j] = Character.getNumericValue(inputArr[i][j]);
                     if (!SudokuHelper.isValid(board, i, j, board[i][j])) {
                         Exception e = new InputMismatchException();
-                        System.out.println("Невозможно создать судоку, введены неккорректные данные: " + e.getMessage());
+                        String mes = "Невозможно создать судоку, введены неккорректные данные: " + e.getMessage();
+                        logger.error(mes);
+                        System.out.println(mes);
                     }
                 }
             }
         }
+        logger.info("Успешное создание объекта класса 'Судоку' из массива символов");
     }
 
     public Sudoku(int[][] inputArr) {
@@ -31,10 +38,14 @@ public class Sudoku {
             for (int j = 0; j < MAX_LENGTH; j++) {
                 board[i][j] = inputArr[i][j];
                 if (!SudokuHelper.isValid(board, i, j, board[i][j])) {
-                    throw new InputMismatchException();
+                    Exception e = new InputMismatchException();
+                    String mes = "Невозможно создать судоку, введены неккорректные данные: " + e.getMessage();
+                    logger.error(mes);
+                    System.out.println(mes);
                 }
             }
         }
+        logger.info("Успешное создание объекта класса 'Судоку' из массива целых чисел");
     }
 
 
